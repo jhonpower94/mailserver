9b50c79c138a0a8b290f44e4eca36927ef528f87
jhon <jhonsnow751@gmail.com>
2026-03-12 04:10:23 +0100

    DvcdihcjkdlscSsfcsdsfef

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post("/send", upload.array("attachments"), async (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
